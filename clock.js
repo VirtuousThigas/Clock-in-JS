7d1ab587629a19c0e00c09bb9aadf04d25185c67
Thiago Caetano Dantas <127804127+VirtuousThigas@users.noreply.github.com>
2024-08-07 12:50:10 -0300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,19 @@
+const secondHand= document.querySelector('.second-hand');
+const minsHand= document.querySelector('.min-hand');
+const hourHand =document.querySelector('.hour-hand');
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDeegrees =((seconds/60) * 360)+90;
+    secondHand.style.transform= `rotate(${secondsDeegrees}deg)`;
+
+    const mins= now.getMinutes();
+    const minsDeegrees= ((mins/60)*360)+((seconds/60)*6)+90;
+    hourHand.style.transform= `rotate(${minsDeegrees}deg)`;
+
+    const hours= now.getHours();
+    const hoursDeegrees= ((hours/12)*360)+((mins/60)*30)+90;
+    hourHand.style.transform= `rotate(${hoursDeegrees}deg)`;
+}
+setInterval(setDate, 1000);
+setDate();
